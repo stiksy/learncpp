89da961ed47e23a1fe8d5dfc7f9aa0c95a8916e2
Fernando Levi Marquardt <fernando.marquardt@king.com>
2023-03-14 14:19:36 +0000

    Local variables

--- course1_section3/main.cpp
+++ course1_section3/main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 
-void print(const char *message);
-
-void print(const char *message) {
-    using namespace std;
-    cout << message << endl;
-}
+using std::cout;
 
 int main() {
-    print("Hello, World!");
+    cout << "Hello, World!" << '\n';
+    cout << 34 << '\n';
+    cout << '\n' << '\n';
+
+    int i = 2 + 2;
+    cout << i << '\n';
+    i = i * 3;
+    cout << i << '\n';
+
+    int j = 2;
+    cout << j << '\n';
+
+    i = 4.8;
+    j = 7 / 2;
+    cout << i << " " << j << '\n';
+
+    float f = 4.9;
+    cout << f << '\n';
+
+    f = 9.0 / 5;
+    cout << f << '\n';
+
     return 0;
 }
