61329b296e07e7aa79fc9a978a2ac3fdc32471d9
Fernando Levi Marquardt <fernando.marquardt@king.com>
2023-03-20 17:52:33 +0000

    Module 4 start

---
+++ course1_module4/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+    int firstNumber;
+    int secondNumber;
+    cout << "Enter a number: ";
+    cin >> firstNumber;
+    cout << "You entered " << firstNumber << ". Enter a second number: ";
+    cin >> secondNumber;
+    cout << "You entered " << secondNumber << ".\n";
+
+
+    return 0;
+}
