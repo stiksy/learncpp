c197640743701b9765288da25d8b19ea6a60dd05
Fernando Levi Marquardt <fernando.marquardt@king.com>
2023-06-09 14:27:30 +0100

    Module 9

---
+++ course1_module9/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+    cout << "Hello, World!" << '\n';
+    cout << 34 << '\n';
+    cout << '\n' << '\n';
+
+    int i = 2 + 2;
+    cout << i << '\n';
+    i = i * 3;
+    cout << i << '\n';
+
+    int j = 2;
+    cout << j << '\n';
+
+    i = 4.8;
+    i = static_cast<int>(4.8);
+    j = static_cast<int>(7 / 2);
+    cout << i << " " << j << '\n';
+
+    float f = 4.9;
+    cout << f << '\n';
+
+    f = 9.0 / 5;
+    cout << f << '\n';
+
+    cout << "Enter a number: ";
+    cin >> i;
+    cout << i * 2 << '\n';
+
+    return 0;
+}
